3cebfe8343fb66d325bbcfa47107f2198fd3e60d
Yong su <107544831+helplessg@users.noreply.github.com>
2022-07-08 11:07:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -141,6 +141,7 @@ async def start(client, message):
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
+                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔮 ᴏᴛʜᴇʀ ʙᴏᴛs 🔮', url='https//t.me/futurebackups') ] ] ), 
                     protect_content=msg.get('protect', False),
                     )
             except FloodWait as e:
